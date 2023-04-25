37b57c374f95cc987bcd06eede7216782f67d3e6
Web Developer <useraccount@WebDeveloperMBP.local>
2023-04-25 13:28:40 -0500

    V2

--- react/GuideButton.tsx
+++ react/GuideButton.tsx
@@ -1,5 +1,5 @@
-
-import React, { ReactChildren } from 'react';
+import React from 'react';
+import { Link } from 'vtex.render-runtime';
 
 // Styles
 import styles from "./styles.css";
@@ -7,51 +7,63 @@ import styles from "./styles.css";
 interface GuideButtonProps {
   link: string
   imgSrc: string
+  imgSrcFallback: string
   text: string
   newTab: boolean
 }
 
-const GuideButton: StorefrontFunctionComponent<GuideButtonProps> = ({ link, imgSrc, text, newTab }) => {
-
-  if (!imgSrc) return <></>
-  if (!link) return <></>
+const GuideButton: StorefrontFunctionComponent<GuideButtonProps> = ({ link, imgSrc, text, newTab, imgSrcFallback }) => {
+  if (!imgSrcFallback && !imgSrc) return <></>;
+  if (!link) return <></>;
 
   return (
-    <div className={styles.buttonContainer}>
+    <Link href={link} target={newTab ? "_blank" : "_self"} className={styles.button}>
       <div className={styles.buttonWrapper}>
-        <a href={link} target={newTab ? "_blank" : "_self"} className={styles.button}>
-          {text && <p className={styles.buttonText}>{text}</p>}
-          <img src={imgSrc} alt="" className={styles.buttonImage} />
-        </a>
+        <img src={imgSrcFallback || imgSrc}
+          alt=""
+          width={250}
+          height={110}
+          className={styles.buttonImage}
+          loading="lazy" />
+        {text && <div className={styles.buttonText}>{text}</div>}
       </div>
-    </div>
+    </Link>
   )
 }
 
 GuideButton.schema = {
-  title: 'Guide Button',
-  description: '',
-  type: 'object',
+  title: "Guide Button",
+  type: "object",
   properties: {
+    imgSrc: {
+      title: "250px 110px - Image",
+      type: "string",
+      widget: {
+        "ui:widget": "image-uploader"
+      }
+    },
+    imgSrcFallback: {
+      title: "Fallback Image",
+      description: "OPTIONAL | If VTEX uploader won't work, paste the image source here.",
+      type: "string"
+    },
     text: {
       title: "Button Text",
       description: "Optional - Text overlayed on top of image.",
-      type: "string"
-    },
-    imgSrc: {
-      title: "Image Source",
-      description: "REQUIRED - Absolute path to image.",
-      type: "string"
+      type: "string",
+      widget: { "ui:widget": "textarea" }
     },
     link: {
       title: "Link",
-      description: "REQUIRED - Absolute or Relative path url.",
-      type: "string"
+      description: "Required - Absolute or Relative path url.",
+      type: "string",
+      widget: { "ui:widget": "textarea" }
     },
     newTab: {
       title: "Open in new tab?",
       type: "boolean"
-    }
+    },
+
   }
 }
 
